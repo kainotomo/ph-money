213e05e508a7ba451f5dbdebfca3aade2b51f935
Panayiotis Halouvas <phalouvas@kainotomo.com>
2019-06-01 08:31:15 +0300

    removed gzip from building

--- build/build_phmoney_4.cmd
+++ build/build_phmoney_4.cmd
@@ -1,2 +1,2 @@
 cd C:\intergo\local\docker
-docker-compose exec -T --workdir=/code_ph-money/build/ php-fpm php build_phmoney.php --step=2 --exclude-zip --exclude-bzip2
+docker-compose exec -T --workdir=/code_ph-money/build/ php-fpm php build_phmoney.php --step=2 --exclude-gzip --exclude-zip --exclude-bzip2
